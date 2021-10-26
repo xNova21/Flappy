7f618620f9d6f3a9ed02a23d9dfe8efc3b7168d8
Lucía <luciazgz93@gmail.com>
2021-10-26 15:50:47 +0200

    final

--- api/game.js
+++ api/game.js
@@ -6,7 +6,6 @@ router.post("/getScore", async (req, res) => {
 await Puntuacion.create(req.body);
   let scores = await Puntuacion.find().sort({score: -1}).limit(3)
   console.log(scores)
-  // scores.sort((a, b) => b.score - a.score);
   res.json({ scores });
 });
 module.exports = router
